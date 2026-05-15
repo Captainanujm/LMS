c681a08b83ae3140825d17821c1f6f40f3bd2d93
Anuj mishra <captainanuj2004@gmail.com>
2026-05-16 00:08:57 +0530

    Update API URL to production

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://lms-2-zj5o.onrender.com/api",
 });
 
 // attach token to every request if available
